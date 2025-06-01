c7578800359f433e2e7fd6099c7c8f3d1478ec89
JMJ <ghtntkdnsk@naver.com>
2025-06-01 16:03:03 +0900

    first commit

---
+++ src/main/java/drive_only/drive_only_server/DriveOnlyServerApplication.java
@@ -0,0 +1,13 @@
+package drive_only.drive_only_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveOnlyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveOnlyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/drive_only/drive_only_server/DriveOnlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package drive_only.drive_only_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveOnlyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
