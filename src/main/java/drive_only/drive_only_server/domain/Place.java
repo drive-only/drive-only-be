263ab7aa965b7fb8fb672a0928e9b17506d298fe
JMJ <ghtntkdnsk@naver.com>
2025-06-25 15:47:55 +0900

    feat: Place 엔티티 생성

--- src/main/java/drive_only/drive_only_server/domain/Place.java
+++ src/main/java/drive_only/drive_only_server/domain/Place.java
@@ -1,4 +1,33 @@
 package drive_only.drive_only_server.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Place {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private String thumbNailUrl;
+    private String useTime;
+    private String restDate;
+    private String phoneNum;
+
+    protected Place() {
+    }
+
+    public Place(String name, String address, String thumbNailUrl, String useTime, String restDate, String phoneNum) {
+        this.name = name;
+        this.address = address;
+        this.thumbNailUrl = thumbNailUrl;
+        this.useTime = useTime;
+        this.restDate = restDate;
+        this.phoneNum = phoneNum;
+    }
 }
