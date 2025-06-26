6d3e937a8f4b97b4f796cecb5dffd824dcb664e8
JMJ <ghtntkdnsk@naver.com>
2025-06-26 16:51:49 +0900

    feat: Course와 Place 엔티티의 중간 엔티티인 CoursePlace 생성

--- src/main/java/drive_only/drive_only_server/domain/CoursePlace.java
+++ src/main/java/drive_only/drive_only_server/domain/CoursePlace.java
@@ -1,4 +1,44 @@
 package drive_only.drive_only_server.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class CoursePlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String placeType;
+    private String content;
+    private int sequence;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    protected CoursePlace() {
+    }
+
+    public CoursePlace(String name, String placeType, String content, int sequence) {
+        this.name = name;
+        this.placeType = placeType;
+        this.content = content;
+        this.sequence = sequence;
+    }
 }
