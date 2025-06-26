9c65e6e7ee735f97584cd59bfec4e67a9c91a3fe
JMJ <ghtntkdnsk@naver.com>
2025-06-26 23:10:12 +0900

    feat: TourAPI와 연동하여 데이터들을 가져올 때 필요한 DTO 생성

---
+++ src/main/java/drive_only/drive_only_server/dto/PlaceDataInitResponse.java
@@ -0,0 +1,43 @@
+package drive_only.drive_only_server.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlaceDataInitResponse {
+    private Response response;
+
+    @Getter
+    @Setter
+    public static class Response {
+        private Body body;
+    }
+
+    @Getter
+    @Setter
+    public static class Body {
+        private Items items;
+    }
+
+    @Getter
+    @Setter
+    public static class Items {
+        private List<Item> item;
+    }
+
+    @Getter
+    @Setter
+    public static class Item {
+        private String addr1;
+        private String addr2;
+        private int contentid;
+        private int contenttypeid;
+        private int createdtime;
+        private String firstimage2;
+        private Double mapx;
+        private Double mapy;
+        private String tel;
+    }
+}
