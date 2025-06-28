86867823cf7539e08676477ed0176bb693adbcca
JMJ <ghtntkdnsk@naver.com>
2025-06-28 20:01:36 +0900

    feat: DefaultUriBuilderFactory의 인코딩 모드를 바꾸어 이중 인코딩 방지

--- src/main/java/drive_only/drive_only_server/config/WebClientConfig.java
+++ src/main/java/drive_only/drive_only_server/config/WebClientConfig.java
@@ -2,14 +2,23 @@ package drive_only.drive_only_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class WebClientConfig {
+    private final static String BASE_URL = "http://apis.data.go.kr/B551011/KorService2";
+
     @Bean
     public WebClient webClient() {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
         return WebClient.builder()
-                .baseUrl("http://apis.data.go.kr/B551011/KorService2")
+                .uriBuilderFactory(factory)
+                .baseUrl(BASE_URL)
+                .defaultHeader(org.springframework.http.HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
