647e73ee317ede418e092d139516106b8e06c866
JMJ <ghtntkdnsk@naver.com>
2025-06-29 03:11:10 +0900

    refactor: TourAPI에서 제공하는 응답값의 타입에 맞게 String으로 변환 및 RequiredArgsConstructor대신 NoArgsConstructor로 변경

--- src/main/java/drive_only/drive_only_server/dto/PlaceDataInitResponse.java
+++ src/main/java/drive_only/drive_only_server/dto/PlaceDataInitResponse.java
@@ -2,21 +2,25 @@ package drive_only.drive_only_server.dto;
 
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class PlaceDataInitResponse {
     private Response response;
 
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Response {
         private Body body;
     }
 
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Body {
         private Items items;
         private int totalCount;
@@ -24,21 +28,23 @@ public class PlaceDataInitResponse {
 
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Items {
         private List<Item> item;
     }
 
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class Item {
         private String addr1;
         private String addr2;
-        private int contentid;
-        private int contenttypeid;
-        private int createdtime;
+        private String contentid;
+        private String contenttypeid;
+        private String createdtime;
         private String firstimage2;
-        private Double mapx;
-        private Double mapy;
+        private String mapx;
+        private String mapy;
         private String tel;
         private String title;
     }
