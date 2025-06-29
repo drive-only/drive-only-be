b3ff6400e9ce865e69c33dcc479a5d49b04d3744
JMJ <ghtntkdnsk@naver.com>
2025-06-29 23:42:04 +0900

    refactor: CourseService 로직들을 가독성 좋게 변경

--- src/main/java/drive_only/drive_only_server/service/course/CourseService.java
+++ src/main/java/drive_only/drive_only_server/service/course/CourseService.java
@@ -9,6 +9,7 @@ import drive_only.drive_only_server.dto.course.CourseCreateResponse;
 import drive_only.drive_only_server.dto.coursePlace.CoursePlaceCreateRequest;
 import drive_only.drive_only_server.repository.CoursePlaceRepository;
 import drive_only.drive_only_server.repository.CourseRepository;
+import drive_only.drive_only_server.repository.MemberReporitory;
 import drive_only.drive_only_server.repository.PlaceRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,11 +23,13 @@ public class CourseService {
     private final CourseRepository courseRepository;
     private final PlaceRepository placeRepository;
     private final CoursePlaceRepository coursePlaceRepository;
+    private final MemberReporitory memberReporitory;
 
-    public CourseService(CourseRepository courseRepository, PlaceRepository placeRepository, CoursePlaceRepository coursePlaceRepository) {
+    public CourseService(CourseRepository courseRepository, PlaceRepository placeRepository, CoursePlaceRepository coursePlaceRepository, MemberReporitory memberReporitory) {
         this.courseRepository = courseRepository;
         this.placeRepository = placeRepository;
         this.coursePlaceRepository = coursePlaceRepository;
+        this.memberReporitory = memberReporitory;
     }
 
     @Transactional
@@ -34,33 +37,47 @@ public class CourseService {
         List<CoursePlace> coursePlaces = new ArrayList<>();
 
         for (CoursePlaceCreateRequest coursePlaceCreateRequest : request.getCoursePlaces()) {
-            Place place = placeRepository.findById(Long.parseLong(coursePlaceCreateRequest.getPlaceId()))
-                    .orElseThrow(() -> new IllegalArgumentException("해당 장소를 찾을 수 없습니다."));
-            CoursePlace coursePlace = new CoursePlace(
-                    coursePlaceCreateRequest.getName(),
-                    coursePlaceCreateRequest.getPlaceType(),
-                    coursePlaceCreateRequest.getContent(),
-                    coursePlaceCreateRequest.getSequence(),
-                    place);
+            Place place = findPlace(coursePlaceCreateRequest);
+            CoursePlace coursePlace = createCoursePlace(coursePlaceCreateRequest, place);
             coursePlaces.add(coursePlace);
         }
 
-        Member mockMember = new Member("email", "nickname", "url", "provider");
+        Member testMember = createMember();
+        Course course = createCourse(request, testMember, coursePlaces);
+        return new CourseCreateResponse(course.getId(), "게시글이 성공적으로 등록되었습니다.");
+    }
 
-        Course course = new Course(
+    private Place findPlace(CoursePlaceCreateRequest coursePlaceCreateRequest) {
+        return placeRepository.findById(Long.parseLong(coursePlaceCreateRequest.getPlaceId()))
+                .orElseThrow(() -> new IllegalArgumentException("해당 장소를 찾을 수 없습니다."));
+    }
+
+    private CoursePlace createCoursePlace(CoursePlaceCreateRequest coursePlaceCreateRequest, Place place) {
+        CoursePlace coursePlace = new CoursePlace(
+                coursePlaceCreateRequest.getName(),
+                coursePlaceCreateRequest.getPlaceType(),
+                coursePlaceCreateRequest.getContent(),
+                coursePlaceCreateRequest.getSequence(),
+                place);
+        coursePlaceRepository.save(coursePlace);
+        return coursePlace;
+    }
+
+    private Member createMember() {
+        Member mockMember = new Member("email", "nickname", "url", "provider");
+        memberReporitory.save(mockMember);
+        return mockMember;
+    }
+
+    private Course createCourse(CourseCreateRequest request, Member testMember, List<CoursePlace> coursePlaces) {
+        Course course = Course.createCourse(
                 request.getTitle(),
                 LocalDate.now(),
                 request.getRecommendation(), request.getDifficulty(),
                 0, 0, 0, false,
-                mockMember, //TODO : 나중에 Member 생성되면 LoginMember(현재 로그인 된 사용자)로 변경
+                testMember, //TODO : 나중에 Member 생성되면 LoginMember(현재 로그인 된 사용자)로 변경
                 coursePlaces
         );
-
-        for (CoursePlace coursePlace : coursePlaces) {
-            coursePlace.setCourse(course);
-        }
-
-        Course savedCourse = courseRepository.save(course);
-        return new CourseCreateResponse(savedCourse.getId(), "게시글이 성공적으로 등록되었습니다.");
+        return courseRepository.save(course);
     }
 }
