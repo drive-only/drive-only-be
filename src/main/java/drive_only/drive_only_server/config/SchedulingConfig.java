a9ec43e7d4694f5839d752e3d1c49779ac189af6
JMJ <ghtntkdnsk@naver.com>
2025-06-30 04:10:41 +0900

    fix: @Scheduled 애노테이션을 Test에서 SchedulingConfig로 옮김

---
+++ src/main/java/drive_only/drive_only_server/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package drive_only.drive_only_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
