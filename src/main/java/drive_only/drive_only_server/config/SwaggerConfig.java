76407638a5ba42f87df77d09bd3e21f430572f79
JMJ <ghtntkdnsk@naver.com>
2025-07-03 19:03:30 +0900

    feat: swagger관련 설정 정보 추가

--- src/main/java/drive_only/drive_only_server/config/SwaggerConfig.java
+++ src/main/java/drive_only/drive_only_server/config/SwaggerConfig.java
@@ -1,4 +1,27 @@
 package drive_only.drive_only_server.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi driveOnlyApi() {
+        return GroupedOpenApi.builder()
+                .group("drive-only-api")
+                .pathsToMatch("/api/**") // /api로 시작하는 경로만 Swagger 문서에 포함
+                .build();
+    }
+
+    @Bean
+    public OpenAPI driveOnlyOpenAPI() {
+        return new OpenAPI().info(
+                new Info()
+                        .title("Drive-Only API")
+                        .version("v1")
+                        .description("Drive-Only API 명세서"));
+    }
 }
