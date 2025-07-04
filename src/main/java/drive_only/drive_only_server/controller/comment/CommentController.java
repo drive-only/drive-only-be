c1c71afe13174f57b241e3e962c594494b23ce6e
JMJ <ghtntkdnsk@naver.com>
2025-07-04 17:47:53 +0900

    feat: 댓글 조회 및 등록 관련 @ApiResponses 설정

--- src/main/java/drive_only/drive_only_server/controller/comment/CommentController.java
+++ src/main/java/drive_only/drive_only_server/controller/comment/CommentController.java
@@ -6,6 +6,9 @@ import drive_only.drive_only_server.dto.comment.search.CommentListResponse;
 import drive_only.drive_only_server.dto.comment.search.CommentSearchResponse;
 import drive_only.drive_only_server.service.comment.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +26,11 @@ public class CommentController {
     private final CommentService commentService;
 
     @Operation(summary = "댓글 조회", description = "댓글 및 대댓글들을 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "댓글 및 대댓글 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "해당 댓글을 찾을 수 없음", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    })
     @GetMapping("/api/courses/{courseId}/comments")
     public ResponseEntity<CommentListResponse> searchComments(
             @PathVariable Long courseId,
@@ -34,6 +42,11 @@ public class CommentController {
     }
 
     @Operation(summary = "댓글 등록", description = "새로운 댓글을 등록")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "댓글 등록 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    })
     @PostMapping("/api/courses/{courseId}/comments")
     public ResponseEntity<CommentCreateResponse> createComment(@PathVariable Long courseId, @RequestBody CommentCreateRequest request) {
         CommentCreateResponse response = commentService.createComment(courseId, request);
