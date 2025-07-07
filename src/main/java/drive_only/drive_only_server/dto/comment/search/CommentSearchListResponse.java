8878dbe779d2b897cfe7dcf5c37aa482fd22ec64
JMJ <ghtntkdnsk@naver.com>
2025-07-08 01:03:01 +0900

    refactor: 댓글 조회용 리스트 DTO의 이름 변경

--- src/main/java/drive_only/drive_only_server/dto/comment/search/CommentSearchListResponse.java
+++ src/main/java/drive_only/drive_only_server/dto/comment/search/CommentSearchListResponse.java
@@ -3,7 +3,7 @@ package drive_only.drive_only_server.dto.comment.search;
 import drive_only.drive_only_server.dto.meta.Meta;
 import java.util.List;
 
-public record CommentListResponse(
+public record CommentSearchListResponse(
         List<CommentSearchResponse> data,
         Meta meta
 ) {}
