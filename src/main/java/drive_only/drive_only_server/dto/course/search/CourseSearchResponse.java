0e2e013b865f08f3268306f4dd05d7486fb0c32d
JMJ <ghtntkdnsk@naver.com>
2025-07-10 03:18:24 +0900

    feat: 코스(게시글) 리스트 조회 관련 응답 DTO 구현

--- src/main/java/drive_only/drive_only_server/dto/course/search/CourseSearchResponse.java
+++ src/main/java/drive_only/drive_only_server/dto/course/search/CourseSearchResponse.java
@@ -1,6 +1,17 @@
 package drive_only.drive_only_server.dto.course.search;
 
+import drive_only.drive_only_server.dto.category.CategoryResponse;
+import java.util.List;
+
 public record CourseSearchResponse(
-        
+        Long courseId,
+        String nickname,
+        String createdDate,
+        String title,
+        String thumbnailUrl,
+        List<String> coursePlaceNames,
+        CategoryResponse categoryResponse,
+        int likeCount,
+        int viewCount
 ) {
 }
