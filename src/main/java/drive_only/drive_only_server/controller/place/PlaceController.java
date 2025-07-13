15ca4cdbe89eab1d0dfc6bb3b2e98b7cc954f042
JMJ <ghtntkdnsk@naver.com>
2025-07-13 17:09:57 +0900

    feat: 장소 리스트 조회 관련 swagger 설정

--- src/main/java/drive_only/drive_only_server/controller/place/PlaceController.java
+++ src/main/java/drive_only/drive_only_server/controller/place/PlaceController.java
@@ -4,6 +4,10 @@ import drive_only.drive_only_server.dto.common.PaginatedResponse;
 import drive_only.drive_only_server.dto.place.PlaceSearchRequest;
 import drive_only.drive_only_server.dto.place.PlaceSearchResponse;
 import drive_only.drive_only_server.service.place.PlaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlaceController {
     private final PlaceService placeService;
 
+    @Operation(summary = "장소 리스트 조회", description = "조건에 따른 장소 리스트 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "장소 리스트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    })
     @GetMapping("/api/places")
     public ResponseEntity<PaginatedResponse<PlaceSearchResponse>> searchPlaces(
             PlaceSearchRequest request,
