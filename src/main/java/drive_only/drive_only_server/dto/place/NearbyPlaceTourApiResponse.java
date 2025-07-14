7f8004fbb755ed0e6efd4159dfbe991933a35ec3
JMJ <ghtntkdnsk@naver.com>
2025-07-15 02:19:29 +0900

    feat: TourAPI로부터 위치기반 주변장소를 가져오는 응답 DTO 생성 및 구현

--- src/main/java/drive_only/drive_only_server/dto/place/NearbyPlaceTourApiResponse.java
+++ src/main/java/drive_only/drive_only_server/dto/place/NearbyPlaceTourApiResponse.java
@@ -1,4 +1,44 @@
 package drive_only.drive_only_server.dto.place;
 
-public record NearbyPlaceTourApiResponse() {
+import drive_only.drive_only_server.dto.data.PlaceDataInitResponse;
+import drive_only.drive_only_server.dto.data.PlaceDataInitResponse.Body;
+import drive_only.drive_only_server.dto.data.PlaceDataInitResponse.Item;
+import drive_only.drive_only_server.dto.data.PlaceDataInitResponse.Items;
+import drive_only.drive_only_server.dto.data.PlaceDataInitResponse.Response;
+import java.util.List;
+
+public record NearbyPlaceTourApiResponse(
+    Response response
+) {
+    public record Response(
+            Body body
+    ) {
+    }
+
+    public record Body(
+            Items items,
+            int totalCount
+    ) {
+    }
+
+    public record Items(
+            List<Item> item
+    ) {
+    }
+
+    public record Item(
+            String addr1,
+            String addr2,
+            String lDongRegnCd,
+            String lDongSignguCd,
+            String contentid,
+            String contenttypeid,
+            String createdtime,
+            String firstimage2,
+            String mapx,
+            String mapy,
+            String tel,
+            String title
+    ) {
+    }
 }
