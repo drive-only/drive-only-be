3be8f37613ebbd6bd9db1cfb62e06c4713872c5e
Dongwook Kim <dongwook258@naver.com>
2025-07-20 14:25:16 +0900

    refactor: 회원 Swagger 정리 및 API 엔드포인트 명시 변경

--- src/main/java/drive_only/drive_only_server/controller/member/MemberController.java
+++ src/main/java/drive_only/drive_only_server/controller/member/MemberController.java
@@ -4,6 +4,11 @@ import drive_only.drive_only_server.domain.Member;
 import drive_only.drive_only_server.domain.ProviderType;
 import drive_only.drive_only_server.dto.member.MemberResponse;
 import drive_only.drive_only_server.service.Member.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/members")
+@Tag(name = "회원", description = "회원 관련 API")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/me")
+    @Operation(summary = "마이페이지", description = "마이페이지 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "마이페이지 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 쿼리 파라미터", content = @Content),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 JWT access token", content = @Content),
+            @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
+    })
+    @GetMapping("/api/members/me")
     public ResponseEntity<MemberResponse> getMyProfile(Authentication authentication) {
         String email = (String) authentication.getPrincipal();
         String providerString = (String) authentication.getAuthorities().stream()
