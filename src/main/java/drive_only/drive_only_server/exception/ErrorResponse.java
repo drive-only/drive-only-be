58bc91a8e50bc039803cc52bd210c5b1652c2a08
JMJ <ghtntkdnsk@naver.com>
2025-08-02 23:45:45 +0900

    hotfix: ErrorResponse의 필드 구성 변경

--- src/main/java/drive_only/drive_only_server/exception/ErrorResponse.java
+++ src/main/java/drive_only/drive_only_server/exception/ErrorResponse.java
@@ -1,22 +1,15 @@
 package drive_only.drive_only_server.exception;
 
 import drive_only.drive_only_server.exception.errorcode.ErrorCode;
-import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "에러 응답")
 public record ErrorResponse(
-        @Schema(description = "에러 코드", example = "INVALID_TOKEN")
         String code,
-
-        @Schema(description = "에러 메시지", example = "유효하지 않은 토큰입니다.")
-        String message,
-        int status
+        String message
 ) {
     public static ErrorResponse of(ErrorCode errorCode) {
         return new ErrorResponse(
                 errorCode.getCode(),
-                errorCode.getMessage(),
-                errorCode.getStatus().value()
+                errorCode.getMessage()
         );
     }
 }
