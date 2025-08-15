5a4e5dd70edadf7fa28d900d2f73dc528682ab86
JMJ <ghtntkdnsk@naver.com>
2025-08-15 19:23:21 +0900

    refactor: Place 관련 응답 DTO 형식 통일

--- src/main/java/drive_only/drive_only_server/success/SuccessCode.java
+++ src/main/java/drive_only/drive_only_server/success/SuccessCode.java
@@ -20,7 +20,14 @@ public enum SuccessCode {
     SUCCESS_CREATE_COMMENT(HttpStatus.CREATED, "댓글이 성공적으로 등록되었습니다."),
     SUCCESS_UPDATE_COMMENT(HttpStatus.OK, "댓글이 성공적으로 수정되었습니다."),
     SUCCESS_DELETE_COMMENT(HttpStatus.OK, "댓글이 성공적으로 삭제되었습니다."),
-    SUCCESS_TOGGLE_COMMENT_LIKE(HttpStatus.OK, "댓글 좋아요 상태가 변경되었습니다.");
+    SUCCESS_TOGGLE_COMMENT_LIKE(HttpStatus.OK, "댓글 좋아요 상태가 변경되었습니다."),
+
+    // Place
+    SUCCESS_GET_PLACES(HttpStatus.OK, "장소 목록을 불러오는 데 성공했습니다."),
+    SUCCESS_GET_NEARBY_PLACES(HttpStatus.OK, "주변 장소 목록을 불러오는 데 성공했습니다."),
+    SUCCESS_GET_SAVED_PLACES(HttpStatus.OK, "저장한 장소 목록을 불러오는 데 성공했습니다."),
+    SUCCESS_SAVE_PLACE(HttpStatus.CREATED, "장소가 성공적으로 저장되었습니다."),
+    SUCCESS_DELETE_SAVED_PLACE(HttpStatus.OK, "저장한 장소가 성공적으로 삭제되었습니다.");
 
     private final HttpStatus status;
     private final String message;
