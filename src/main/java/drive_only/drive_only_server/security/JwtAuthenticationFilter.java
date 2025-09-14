a273319042b821fdae6610d859891d67bb1ef0a0
JMJ <ghtntkdnsk@naver.com>
2025-09-14 13:08:56 +0900

    refactor: JwtAuthenticationFilter에 logout도 추가

--- src/main/java/drive_only/drive_only_server/security/JwtAuthenticationFilter.java
+++ src/main/java/drive_only/drive_only_server/security/JwtAuthenticationFilter.java
@@ -38,6 +38,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         boolean isPermitAll =
                 "OPTIONS".equalsIgnoreCase(method) ||
                         path.startsWith("/api/login") ||
+                        path.startsWith("/api/logout") ||
                         path.startsWith("/api/auth/") ||
                         path.startsWith("/swagger-ui/") ||
                         path.startsWith("/v3/api-docs/") ||
