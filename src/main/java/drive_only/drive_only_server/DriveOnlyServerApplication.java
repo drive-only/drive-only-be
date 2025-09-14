4ac4c890729fd17e31fbd385224c9fb9a38256dc
JMJ <ghtntkdnsk@naver.com>
2025-09-14 12:44:47 +0900

    RUN:EC2

--- src/main/java/drive_only/drive_only_server/DriveOnlyServerApplication.java
+++ src/main/java/drive_only/drive_only_server/DriveOnlyServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DriveOnlyServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DriveOnlyServerApplication.class, args);
     }
